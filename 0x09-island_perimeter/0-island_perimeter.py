ad41e1250c1d2b3bad2d5e04cf35b19bced29f80
Sjallow <seringj54@gmail.com>
2023-11-29 15:53:00 -0800

    islands

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,28 +1,66 @@
 #!/usr/bin/python3
-"""Prime game module.
-"""
+""" Island Perimeter mandatory """
 
 
-def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds.
+def test0(n):
     """
-    if x < 1 or not nums:
-        return None
-    marias_wins, bens_wins = 0, 0
-    # generate primes with a limit of the maximum number in nums
-    n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if i == 1 or not is_prime:
-            continue
-        for j in range(i + i, n + 1, i):
-            primes[j - 1] = False
-    # filter the number of primes less than n in nums for each round
-    for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-    if marias_wins == bens_wins:
-        return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
+    *******************************
+    ******test if n is 0 or 1******
+    *******************************
+    @n: number of the grid
+    Returns: 0 if n is 1 or 1 if n is 0
+    """
+    if (n == 0):
+        return 1
+    return 0
+
+
+def island_perimeter(grid):
+    """
+    ***********************************************
+    ******returns the perimeter of the island******
+    ***************described in grid***************
+    ***********************************************
+    @grid: is a list of list of integers:
+                *** 0 represents water
+                *** 1 represents land
+                *** Each cell is square, with a side
+                    length of 1
+                *** Cells are connected horizontally/vertically
+                    (not diagonally).
+                *** grid is rectangular, with its width and
+                    height not exceeding 100
+            The grid is completely surrounded by water
+            There is only one island (or nothing).
+            The island doesn’t have “lakes” (water inside that isn’t
+                connected to the water surrounding the island).
+    """
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+
+    p = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
+            if grid[i][j] == 1:
+                if i-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i-1][j])
+                if j-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i][j-1])
+
+                try:
+                    p += test0(grid[i+1][j])
+                except IndexError:
+                    p += 1
+                try:
+                    p += test0(grid[i][j+1])
+                except IndexError:
+                    p += 1
+
+    return p
